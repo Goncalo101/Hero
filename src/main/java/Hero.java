7c448da449e4b84c684056f215678511df143934
Goncalo101 <goncalo.oli@outlook.com>
2019-03-01 11:47:47 +0000

    Added Element class to generalize the Hero and Wall classes

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -3,37 +3,18 @@ import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 
-public class Hero {
-    protected Position position;
-
+public class Hero extends Element {
     public Hero(int x, int y) {
-        position = new Position(x, y);
+        super(x, y);
     }
 
-
-    public Position moveUp() {
-        return new Position(position.getX(), position.getY() - 1);
-    }
-
-    public Position moveDown() {
-        return new Position(position.getX(), position.getY() + 1);
-    }
-
-    public Position moveLeft() {
-        return new Position(position.getX() - 1, position.getY());
-    }
-
-    public Position moveRight() {
-        return new Position(position.getX() + 1, position.getY());
-    }
-
-    public void draw(TextGraphics screen) {
-        screen.setForegroundColor(TextColor.Factory.fromString("#FFFF33"));
-        screen.enableModifiers(SGR.BOLD);
-        screen.putString(new TerminalPosition(position.getX(), position.getY()), "X");
+    public void draw(TextGraphics graphics) {
+        graphics.setForegroundColor(TextColor.Factory.fromString("#FFFF33"));
+        graphics.enableModifiers(SGR.BOLD);
+        graphics.putString(new TerminalPosition(position.getX(), position.getY()), "X");
     }
 
     public void setPosition(Position position) {
-        this.position = position;
+        super.position = position;
     }
 }
--- src/main/java/Wall.java
+++ src/main/java/Wall.java
@@ -3,16 +3,16 @@ import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 
-public class Wall extends Hero {
+public class Wall extends Element {
 
     public Wall(int x, int y) {
         super(x, y);
     }
 
     @Override
-    public void draw(TextGraphics screen) {
-        screen.setForegroundColor(TextColor.Factory.fromString("#FF0033"));
-        screen.enableModifiers(SGR.BOLD);
-        screen.putString(new TerminalPosition(position.getX(), position.getY()), "|");
+    public void draw(TextGraphics graphics) {
+        graphics.setForegroundColor(TextColor.Factory.fromString("#FF0033"));
+        graphics.enableModifiers(SGR.BOLD);
+        graphics.putString(new TerminalPosition(position.getX(), position.getY()), "|");
     }
 }
